90232900fa14b9bf3802a4235b74b216ddff5910
Peter Palfrader <peter@palfrader.org>
2019-12-11 14:02:19 +0100

    gcc complains about shadowing in the easylogging library header file that we include.  Ignore those

--- surf/include/surf.h
+++ surf/include/surf.h
@@ -18,7 +18,10 @@
 #pragma once
 
 #include "config.h"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wshadow"
 #include "../easyloggingpp/src/easylogging++.h"
+#pragma GCC diagnostic pop
 
 #include <assert.h>
 
